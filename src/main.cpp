adc5b0da0cb9ae3452c70211abb22fb6d7787b00
Robin Dubourgeois <rdubourgeois@gmail.com>
2025-07-01 11:48:57 +0200

    double LED blink

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,31 @@
 #include "pinout.h"
 
 // put function declarations here:
+void blink_redLed();
+void blink_greenLed();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(REDLED_PIN, OUTPUT);
+  pinMode(GREENLED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  blink_greenLed();
+  blink_redLed();
+}
+
+void blink_redLed() {
   digitalWrite(REDLED_PIN, HIGH);
   delay(500);
   digitalWrite(REDLED_PIN, LOW);
-  delay(500);  
+  delay(500);
+}
+
+void blink_greenLed() {
+  digitalWrite(GREENLED_PIN, HIGH);
+  delay(500);
+  digitalWrite(GREENLED_PIN, LOW);
+  delay(500);
 }
